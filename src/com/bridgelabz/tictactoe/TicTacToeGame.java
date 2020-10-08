ea2ab4e05595c41c31978bf4c1ac7cde96b44cab
kush16816 <55789306+kush16816@users.noreply.github.com>
2020-10-08 11:33:39 +0530

    Add : Function to show board in 3X3 format

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -4,31 +4,38 @@ package com.bridgelabz.tictactoe;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	
+
 	/**
 	 * @return returns
 	 */
 	public static char[] createBoard() {
 		char board[] = new char[10];
-		for(int boardIndex = 1; boardIndex < 10; boardIndex++)
-		{
+		for (int boardIndex = 1; boardIndex < 10; boardIndex++) {
 			board[boardIndex] = ' ';
 		}
 		return board;
 	}
 
-	public static char[] playChance(char[] board) {
+	public static char[] chooseSymbol(char[] board) {
 		System.out.println("Please enter the choice x or o");
 		Scanner userInput = new Scanner(System.in);
 		String userPlay = userInput.next();
-		char userChance = userPlay.toCharArray()[0];
-		char comPlay = 'x';
-		if(userChance=='x')
-			comPlay = 'o';
+		char userSymbol = userPlay.toCharArray()[0];
+		char compSymbol = 'x';
+		if (userSymbol == 'x')
+			compSymbol = 'o';
 		return board;
 	}
-	public static void main(String [] args) {
+	
+	public static void showBoard(char[] board) {
+		for(int boardIndex = 1; boardIndex < 10; boardIndex += 3) {
+			System.out.println(board[boardIndex] + " " + board[boardIndex+1] + " " + board[boardIndex+2]);
+		}
+	}
+
+	public static void main(String[] args) {
 		char[] board = createBoard();
-		board = playChance(board);
+		board = chooseSymbol(board);
+		showBoard(board);
 	}
 }
